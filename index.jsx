8605334fe34f4fbd2a6abd35c9c88b9acbe23b44
kimdo20 <kimdo7909@gmail.com>
2022-04-12 13:14:31 -0400

    added main files

--- index.jsx
+++ index.jsx
@@ -1,5 +1,8 @@
-// import * as React from 'react';
-// import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+import { ListViewComponent } from '@syncfusion/ej2-react-lists';
+//import "./styles.css";
+
 // //import {}
 // export default class App extends React.Component
 // {
@@ -19,26 +22,45 @@
 //     }
 // }
 
-const App = () => 
-{
-    const [checked, setChecked] = React.useState(false);
+export default class App extends React.Component {
+    constructor() {
+        super(...arguments);
+        //define the array of JSON
+        this.data = [
+            { text: 'Table', id: '1' },
+            { text: 'Bed', id: '2' },
+            { text: 'Closet', id: '3' },
+            { text: 'Dresser', id: '4' },
+        ];
+    }
+    render() {
+        return (
+        // specifies the tag to render the ListView component
+        <ListViewComponent id='list' dataSource={this.data} showCheckBox={true} headerTitle='TO DO LIst' showHeader={true}></ListViewComponent>);
+    }
+}
+ReactDOM.render(<App />, document.getElementById('element'));
 
-    const handleChange = () =>
-    {
-        setChecked(!checked);
-    };
+// const App = () => 
+// {
+//     const [checked, setChecked] = React.useState(false);
 
-    return(
-        <div>
-            <label>
-                <input
-                    type="checkbox"
-                    checked={checked}
-                    onChange={handleChange}
-                />
-                My Value
-            </label>
-            <p>Is "My Value" checked? {checked.toString()}</p>
-        </div>
-    );
-};
+//     const handleChange = () =>
+//     {
+//         setChecked(!checked);
+//     };
+
+//     return(
+//         <div>
+//             <label>
+//                 <input
+//                     type="checkbox"
+//                     checked={checked}
+//                     onChange={handleChange}
+//                 />
+//                 My Value
+//             </label>
+//             <p>Is "My Value" checked? {checked.toString()}</p>
+//         </div>
+//     );
+// };
